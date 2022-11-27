e59e1348fd637f669b719876c2e20a37d0716006
kfaff <s19191@pjwstk.edu.pl>
2022-11-27 19:49:03 +0100

    Finish.

--- Riak/src/main/java/org/example/Task01.java
+++ Riak/src/main/java/org/example/Task01.java
@@ -19,7 +19,15 @@ public class Task01 {
             cluster = RiakConnectionHelper.setUpCluster();
             RiakClient client = new RiakClient(cluster);
 
+            /**
+             * Nowy bucket s19191
+             */
+
             Namespace s19191Bucket = new Namespace("s19191");
+
+            /**
+             * Zapisanie poprzez spreparowanie odpowiedniego Stringa oraz określenie ContentType na application/json
+             */
 
             Location meLocation = new Location(s19191Bucket, "me");
             BinaryValue text = BinaryValue.create("{\n" +
@@ -39,6 +47,10 @@ public class Task01 {
             client.execute(storeMeOp);
             System.out.println("Me saved as String");
 
+            /**
+             * Zapisanie poprzez stworzenie obiektu.
+             */
+
             People Mark = new People("Mark", "Kowalski", 99, true);
             Location markLocation = new Location(s19191Bucket, "mark");
             StoreValue storeMarkOp = new StoreValue.Builder(Mark)
@@ -47,6 +59,11 @@ public class Task01 {
 
             client.execute(storeMarkOp);
             System.out.println("Mark saved as object");
+
+            /**
+             * Zapisanie poprzez stworzenie obiektu. Ten samochód to wynik mojego niedokładnego przeczytania polecenia. Sądziłem że mamy wstawić parę takich obiektów do bazy, a nie tylko jeden.
+             * Natomiast już powstał to niech zostanie. Później jest on nie używany.
+             */
 
             Car Toyota = new Car("Mark", 2010, 7.6);
             Location toyotaLocation = new Location(s19191Bucket, "toyota");
@@ -57,6 +74,10 @@ public class Task01 {
             client.execute(storeToyotaOp);
             System.out.println("Toyota saved as object");
 
+            /**
+             * Odczytanie obiektu mark z bazy danych.
+             */
+
             Location readKey = new Location(new Namespace("s19191"), "mark");
             FetchValue fetch = new FetchValue.Builder(readKey)
                     .withOption(FetchValue.Option.R, new Quorum(3))
@@ -66,24 +87,40 @@ public class Task01 {
             System.out.println("Mark read before update");
             System.out.println(readObj.getValue());
 
+            /**
+             * Uaktualnienie wieku Marka na 50.
+             */
+
             Mark.age = 50;
             PeopleUpdate updatedBook = new PeopleUpdate(Mark);
             UpdateValue updateValue = new UpdateValue.Builder(markLocation)
                     .withUpdate(updatedBook).build();
-            UpdateValue.Response responseForUpdate = client.execute(updateValue);
+            client.execute(updateValue);
 
             System.out.println("Mark updated");
+
+            /**
+             * Odczytanie obiektu mark z bazy danych po uaktualnieniu jego wieku.
+             */
 
             FetchValue.Response readResponseAfterUpdate = client.execute(fetch);
             RiakObject readObjAfterUpdate = readResponseAfterUpdate.getValue(RiakObject.class);
             System.out.println("Mark read after update");
             System.out.println(readObjAfterUpdate.getValue());
 
+            /**
+             * Usunięcie Marka.
+             */
+
             Location geniusQuote = new Location(new Namespace("s19191"), "mark");
             DeleteValue delete = new DeleteValue.Builder(geniusQuote).build();
             client.execute(delete);
 
             System.out.println("Mark deleted");
+
+            /**
+             * Próba odczytania Marka po usunięciu. Dostaniemy null.
+             */
 
             FetchValue.Response readResponseAfterRemoval = client.execute(fetch);
             RiakObject readObjAfterRemoval = readResponseAfterRemoval.getValue(RiakObject.class);
--- Riak/src/main/java/org/example/Task02.java
+++ Riak/src/main/java/org/example/Task02.java
@@ -14,7 +14,6 @@ import com.basho.riak.client.core.query.RiakObject;
 import com.basho.riak.client.core.query.indexes.StringBinIndex;
 import com.basho.riak.client.core.util.BinaryValue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,18 +28,27 @@ public class Task02 {
 
             Namespace part2Bucket = new Namespace("part2");
 
+            /**
+             * Kropka pierwsza oraz druga, wyszukiwanie ludzi o wieku równym 18.
+             */
 
-//            BinIndexQuery biq = new BinIndexQuery.Builder(part2Bucket, "person_age", "18")
-//                    .build();
-//            BinIndexQuery.Response response = client.execute(biq);
-
-            IntIndexQuery iiq = new IntIndexQuery.Builder(part2Bucket, "person_age", 18L)
+            IntIndexQuery iiq_person_age_18 = new IntIndexQuery.Builder(part2Bucket, "person_age", 18L)
                     .build();
-            IntIndexQuery.Response response = client.execute(iiq);
+            IntIndexQuery.Response response_person_age_18 = client.execute(iiq_person_age_18);
+
+            /**
+             * Mapa przechowywująca wszystkie te osoby.
+             */
 
             Map<BinaryValue, RiakObject> peopleAge18 = new HashMap();
-            List<SecondaryIndexQuery.Response.Entry<Long>> entries = response.getEntries();
-            for (BinIndexQuery.Response.Entry entry : entries) {
+
+            List<SecondaryIndexQuery.Response.Entry<Long>> response_person_age_18Entries = response_person_age_18.getEntries();
+
+            /**
+             * Wyszukiwanie osób po ich kluczu, dodanie do hashmapy (klucz, osoba) oraz wypisanie ich imion oraz nazwisk.
+             */
+
+            for (BinIndexQuery.Response.Entry entry : response_person_age_18Entries) {
                 Location readKey = new Location(new Namespace("part2"), entry.getRiakObjectLocation().getKey());
                 FetchValue fetch = new FetchValue.Builder(readKey)
                         .withOption(FetchValue.Option.R, new Quorum(3))
@@ -48,9 +56,12 @@ public class Task02 {
                 FetchValue.Response readResponse = client.execute(fetch);
                 RiakObject readObj = readResponse.getValue(RiakObject.class);
                 peopleAge18.put(entry.getRiakObjectLocation().getKey(), readObj);
-                System.out.println(readObj.getValue());
                 System.out.println(readObj.getValue().toString().split(",")[0].split("\"")[3] + ", " + readObj.getValue().toString().split(",")[1].split("\"")[3]);
             }
+
+            /**
+             * Kropka trzecia, na podstawie hashmapy iterujemy po każdej osobie oraz dodajemy jej index person_status - "dorosly". Na sam koniec zapisujemy, aby index pojawił się w bazie.
+             */
 
             peopleAge18.forEach((key, person) -> {
                 person.getIndexes().getIndex(StringBinIndex.named("person_status")).add("dorosly");
@@ -67,6 +78,72 @@ public class Task02 {
                     throw new RuntimeException(e);
                 }
             });
+
+            /**
+             * Kropka czwarta, wyszukiwanie ludzi o wieku 65, bądź większym.
+             */
+
+            IntIndexQuery iiq_person_age_Above65 = new IntIndexQuery.Builder(part2Bucket, "person_age", 65L, Long.MAX_VALUE)
+                    .build();
+            IntIndexQuery.Response response_person_age_Above65 = client.execute(iiq_person_age_Above65);
+
+            List<SecondaryIndexQuery.Response.Entry<Long>> response_person_age_above65Entries = response_person_age_Above65.getEntries();
+
+            /**
+             * Po kluczu znalezionych osób je pobieramy. Następnie dodajemy im nowy index person_status o wartości "emeryt" oraz ponownie zapisujemy.
+             */
+
+            for (BinIndexQuery.Response.Entry entry : response_person_age_above65Entries) {
+                Location readKey = new Location(new Namespace("part2"), entry.getRiakObjectLocation().getKey());
+                FetchValue fetch = new FetchValue.Builder(readKey)
+                        .withOption(FetchValue.Option.R, new Quorum(3))
+                        .build();
+                FetchValue.Response readResponse = client.execute(fetch);
+
+                RiakObject pensioner = readResponse.getValue(RiakObject.class);
+                pensioner.getIndexes().getIndex(StringBinIndex.named("person_status")).add("emeryt");
+
+                StoreValue store = new StoreValue.Builder(pensioner)
+                        .withLocation(readKey)
+                        .build();
+                try {
+                    client.execute(store);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            /**
+             * Kropka piąta, wyszukiwanie ludzi o indexie person_status - emeryt.
+             */
+
+            BinIndexQuery biq = new BinIndexQuery.Builder(part2Bucket, "person_status", "emeryt")
+                    .build();
+            BinIndexQuery.Response response = client.execute(biq);
+            List<SecondaryIndexQuery.Response.Entry<String>> entries = response.getEntries();
+
+            /**
+             * Wyświetlanie wszystkich emerytów w formacie klucz, wartość.
+             */
+
+            for (BinIndexQuery.Response.Entry entry : entries) {
+                Location readKey = new Location(new Namespace("part2"), entry.getRiakObjectLocation().getKey());
+                FetchValue fetch = new FetchValue.Builder(readKey)
+                        .withOption(FetchValue.Option.R, new Quorum(3))
+                        .build();
+                FetchValue.Response readResponse = client.execute(fetch);
+                RiakObject readObj = readResponse.getValue(RiakObject.class);
+                System.out.println(readObj);
+                System.out.println("Key: " + readKey.getKey() + ", value: " + readObj.getValue());
+            }
+
+            /**
+             * Wyświetlanie liczby wszystkich emerytów.
+             */
+
+            System.out.println("Number of pensioners in database: " + entries.size());
 
             cluster.shutdown();
         } catch (Exception e) {
