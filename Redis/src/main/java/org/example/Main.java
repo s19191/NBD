5bda70848fe42c8655865569be2cf562563cb893
kfaff <s19191@pjwstk.edu.pl>
2022-12-11 05:19:45 +0100

    Done.

--- Redis/src/main/java/org/example/Main.java
+++ Redis/src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import redis.clients.jedis.Jedis;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Jedis jedis = new Jedis("localhost", 6379);
+            System.out.println("Connection to server sucessfully");
+
+            jedis.ltrim("list", -1, 0);
+
+            Scanner scanner = new Scanner(System.in);
+            int counter = 0;
+            String s1 = "";
+
+            System.out.println("Wprowadź dane do listy:");
+            while (!s1.equals("exit") && scanner.hasNext()) {
+                s1 = scanner.next();
+                if (!s1.equals("exit")) {
+                    System.out.println(counter);
+                    if (counter == 4) {
+                        jedis.lpush("list", s1);
+                        System.out.println(jedis.lrange("list", 0, -1));
+                    } else if (counter >= 5) {
+                        jedis.rpop("list");
+                        jedis.lpush("list", s1);
+                        System.out.println(jedis.lrange("list", 0, -1));
+                    } else {
+                        jedis.lpush("list", s1);
+                    }
+                    counter++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
