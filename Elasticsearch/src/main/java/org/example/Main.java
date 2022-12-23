15fc71df9804ce79c568cebc781358d7fff0df4f
kfaff <s19191@pjwstk.edu.pl>
2022-12-23 17:05:11 +0100

    Added dependencies, created simple client.

--- Elasticsearch/src/main/java/org/example/Main.java
+++ Elasticsearch/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        restClient.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
